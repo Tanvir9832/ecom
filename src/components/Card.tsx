31f47481da8912ba41068745c0c17d1f50a901a1
tanviAlam <tanvir9832@gmail.com>
2024-09-07 05:58:31 +0600

    feat: card add

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,22 +1,27 @@
 import image from '../../public/Image.png'
 import Button from './ui/Button'
-import Added from '../../public/Added.png';
+import Added from '../../public/Added.svg';
 
 const Card = () => {
   return (
-    <div>
-      <img src={image} alt="product" />
-      <h2>Name of the product</h2>
-      <div className='flex'>
-        <p>$200</p>
-        <del>$300</del>
-        <p>30% OFF</p>
+    <div className='flex flex-col p-4 border rounded-2xl w-[277px] h-[484px] gap-8'>
+      <div>
+        <img src={image} alt="product" />
       </div>
-      <p>
-      It has a backrest that can be tilted back, and often a footrest extended
-      </p>
+      <div className='flex flex-col gap-3'>
+        <h2 className='text-[#343434] font-semibold text-lg leading-[21.6px] mb-1'>Name of the product</h2>
+        <div className='flex text-lg leading-[21.6px] gap-3 '>
+          <p className='font-bold text-[#343434]'>$200.00</p>
+          <del className='font-medium text-[#ABABAB]'>$300.00</del>
+          <p className='font-semibold text-[#B92E2E]'>30% OFF</p>
+        </div>
+        <p className='font-normal text-sm text-[#838383] leading-[16.8px]'>
+          It has a backrest that can be tilted back, and often a footrest extended
+        </p>
+      </div>
       <Button>
         <img className='text-black' src={Added} alt="" />
+        <p className='font-semibold text-base leading-[19.2px]'>Add to cart</p>
       </Button>
     </div>
   )
